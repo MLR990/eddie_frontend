dcc3c380a4eca6003707d24b62f5f3fa33937150
Michael Reagan <mreagan@assurecare.com>
2024-05-18 13:28:23 -0400

    Add not found page and route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import { lazyImport } from '../utils/lazy-import';
 
 const { LandingRoute } = lazyImport(() => import('./landing'), 'LandingRoute');
-
+const { NotFoundRoute } = lazyImport(
+  () => import('./not-found'),
+  'NotFoundRoute'
+);
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path='*' element={<LandingRoute />} />
+      <Route path='/' element={<LandingRoute />} />
+
+      <Route path='*' element={<NotFoundRoute />} />
     </Routes>
   );
 };
